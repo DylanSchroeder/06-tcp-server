db8fbe05f9b87ea28a36d646e8f5f8e5c37bd8e5
Dylan Schroeder <schrodac@gmail.com>
2018-08-20 22:48:35 -0500

    add all necessary files

--- src/actions/nickname.js
+++ src/actions/nickname.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const events = require('../lib/events');
-const socketPool = require('../lib/socket-pool');
+// const socketPool = require('../lib/socket-pool');
 
 events.on('@nickname', (user, name) => {
   user.nickname = name;
